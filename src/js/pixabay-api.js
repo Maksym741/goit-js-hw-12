002b0800ff470b3677f257d0c1ffca8d00fd1eb7
Pastushyna <151558075+Maksym741@users.noreply.github.com>
2024-05-16 20:09:47 +0200

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,22 @@
+// pixabay-api
+
+const API_KEY = '43826877-794dcc159b12a8b36c7bc4349';
+const BASE_URL = 'https://pixabay.com/api/';
+export const fetchPhotos = searchImage => {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: searchImage,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    per_page: 21,
+  });
+  return fetch(`${BASE_URL}/?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+    }
+    return response.json();
+  });
+};
